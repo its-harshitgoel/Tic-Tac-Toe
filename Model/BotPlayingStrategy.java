5c42350bcde7e1f9880bf0374cf0c0f487fe093e
Harshit Goel <103281383+its-harshitgoel@users.noreply.github.com>
2024-04-23 21:37:15 +0530

    Add files via upload

---
+++ Model/BotPlayingStrategy.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class BotPlayingStrategy {
+
+    public Move makeMove(Board board) {
+        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+    }
+
+}
---
+++ Model/WinningStrategy.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class WinningStrategy {
+
+    public boolean checkWinner(Move move, Board board) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkWinner'");
+    }
+
+}
